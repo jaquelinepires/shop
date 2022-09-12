29cb5abef59d06dc5b2f40ebd0993942038e800a
Jaqueline <piresjaqueline17@gmail.com>
2022-09-12 20:24:09 -0300

    feat: Application header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return < Component {...pageProps} />
+  return (
+  <Container>
+    <Header>
+      <h1>Shop</h1>
+    </Header>
+
+    < Component {...pageProps} />
+  </Container>
+
+  )
 }

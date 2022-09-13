2dc60390674c6111d4a15c0aac31db3890dc7550
Jaqueline <piresjaqueline17@gmail.com>
2022-09-12 22:34:07 -0300

    feat: Creating carousel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,17 +2,16 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
-  minHeight: 656
+  // minHeight: 656
 })
 
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, orange 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  // padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',

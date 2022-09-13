b4c2f60dd45db9a8c213f9506bfda665bf1fbe48
Jaqueline <piresjaqueline17@gmail.com>
2022-09-13 19:07:30 -0300

    feat: Using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import Image from "next/future/image";
 import { HomeContainer, Product } from "../styles/pages/home";
 import { useKeenSlider } from 'keen-slider/react'
-import camiseta1 from '../assets/1.png'
-import camiseta2 from '../assets/2.png'
-import camiseta3 from '../assets/3.png'
-import tomato from '../assets/tomato.png'
-
 import 'keen-slider/keen-slider.min.css'
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
 
@@ -44,7 +39,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -61,7 +56,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 })
   return {
     props: {
-      products
-    }
+      products,
+    },
+    revalidate: 60 * 60 * 2 //2 hours
   }
 }
